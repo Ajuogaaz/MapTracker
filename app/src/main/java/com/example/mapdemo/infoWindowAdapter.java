d536fad9370bd68ab530505714d282ab69b197fe
Linus Brian Okoth <okoth@fb.com>
2020-07-04 13:33:18 -0400

    populated fields

--- app/src/main/java/com/example/mapdemo/infoWindowAdapter.java
+++ app/src/main/java/com/example/mapdemo/infoWindowAdapter.java
@@ -2,6 +2,7 @@ package com.example.mapdemo;
 
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
@@ -19,6 +20,9 @@ class CustomWindowAdpater implements GoogleMap.InfoWindowAdapter {
     public View getInfoWindow(Marker marker) {
         //Getting the view from the layout file
         View view = mInflater.inflate(R.layout.custom_info_window, null);
+
+        //populate fields
+        TextView title = view.findViewById(R.id.tv_info_window_description);
 
 
         return null;
